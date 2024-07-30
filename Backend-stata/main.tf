03b7ad1b3662e6fe71cb983735faf183bf688beb
samuelmalik@yahoo.com <samuelmalik@yahoo.com>
2024-07-30 13:03:18 +0100

    create modules with terraform

--- Backend-stata/main.tf
+++ Backend-stata/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-mobann-jenkins"
+  bucket = "terraform-state-mobann-jenkins-live"
 
   lifecycle {
     prevent_destroy = true
